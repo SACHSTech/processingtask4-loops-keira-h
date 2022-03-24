81ea1d1544a6cf413e202262f0a94519181fa389
Keira Hosey <78433426+keira-h@users.noreply.github.com>
2022-03-24 13:21:16 +0000

    current

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
   /**
@@ -24,12 +24,33 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// Quad 1
+    stroke(0);
+    strokeWeight(2);
+    
+    for (int lineX = 0; lineX <= 200; lineX += 20) {
+      for (int lineY = 0; lineY <= 200; lineY += 20) {
+      line(lineX, 0, lineX, 200); 
+      line(0, lineY, 200, lineY); 
+      }
+      }
+  
+  // Quad 2
+    for (int circleY = 30; circleY <= 175; circleY += 35) {
+    for (int circleX = 230; circleX <= 375; circleX += 35) {
+      fill(255, 0, 0);
+      ellipse(circleX, circleY, 20, 20);
+      }
+      }
+  
+  // Quad 3
+    for(int gradientY = 0; gradientY < 400; gradientY++){
+    for(int gradientX = 0; gradientX < 200; gradientX++){
+      stroke(gradientX, 0);
+      stroke(gradientY, 255);
+      point(gradientX, gradientY);
+    }
+  }
   }
   
   // define other methods down here.
