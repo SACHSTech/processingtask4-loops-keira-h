99f77a33d3c1fbb574a5ea78d5314826f3b35b08
Keira Hosey <78433426+keira-h@users.noreply.github.com>
2022-03-25 13:43:33 +0000

    fixed quad 3

--- Sketch.java
+++ Sketch.java
@@ -26,12 +26,10 @@ public class Sketch extends PApplet {
 	  
 	// Quad 1
     stroke(0);
-    strokeWeight(2);
-    
-    for (int lineX = 0; lineX <= 200; lineX += 20) {
-      for (int lineY = 0; lineY <= 200; lineY += 20) {
-      line(lineX, 0, lineX, 200); 
-      line(0, lineY, 200, lineY); 
+    for (int lineX = 20; lineX <= 190; lineX += 20) {
+      for (int lineY = 20; lineY <= 190; lineY += 20) {
+        line(lineX, 0, lineX, 200); 
+        line(0, lineY, 200, lineY); 
       }
       }
   
@@ -44,13 +42,35 @@ public class Sketch extends PApplet {
       }
   
   // Quad 3
-    for(int gradientY = 0; gradientY < 400; gradientY++){
-    for(int gradientX = 0; gradientX < 200; gradientX++){
+    for(int gradientY = 0; gradientY < 200; gradientY++){
+    for(int gradientX = 200; gradientX < 400; gradientX++){
       stroke(gradientX, 0);
       stroke(gradientY, 255);
-      point(gradientX, gradientY);
+      point(gradientY, gradientX);
     }
   }
+  // Quad 4
+    //fill(50, 168, 82);
+    //ellipse(300, 300, 30, 30);
+    //draw petals
+    //fill(232, 107, 53);
+    //ellipse(300, 260, 20, 70);
+    //rotate((float)1.5);
+    //ellipse(320, 265, 20, 70);
+    //ellipse(50, 150, 20, 70);
+    //ellipse(150, 150, 20, 70);
+    //ellipse(50, 50, 20, 70);
+    //ellipse(150, 50, 20, 70);
+    //ellipse(50, 150, 20, 70);
+    //ellipse(150, 150, 20, 70);
+    
+    //for (int petalY = 300; petalY <= 400; petalY += 50) {
+    //for (int petalX = 300; petalX <= 400; petalX += 50) {
+      //fill(232, 107, 53);
+      //rotate(1);
+      //ellipse(petalX, petalY, 20, 60);
+      //}
+      //}
   }
   
   // define other methods down here.
